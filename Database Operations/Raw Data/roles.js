bbd74b8043fbfb792f6aee77ae659a81c533cd2d
Saad <rishat.5081@gmail.com>
2022-07-18 15:46:02 +0500

    create the raw roles

---
+++ Database Operations/Raw Data/roles.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rolesData: [
+    {
+      roleName: "admin",
+      roleType: "Admin have access to all",
+    },
+    {
+      roleName: "player",
+      roleType: "Player does not have access to all",
+    },
+  ],
+};
