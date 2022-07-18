d9138946d40b8fdd216caa3d2f5a9ddd98d2e364
Saad <rishat.5081@gmail.com>
2022-07-18 20:18:40 +0500

    comment the code

--- server.js
+++ server.js
@@ -78,14 +78,15 @@ app.all("*", async (req, res) => {
   });
 });
 
-process.once("SIGUSR2", function () {
-  process.kill(process.pid, "SIGUSR2");
-});
+// process.once("SIGUSR2", function () {
+//   process.kill(process.pid, "SIGUSR2");
+// });
 
-process.on("SIGINT", function () {
-  // this is only called on ctrl+c, not restart
-  process.kill(process.pid, "SIGINT");
-});
+// process.on("SIGINT", function () {
+//   // this is only called on ctrl+c, not restart
+//   console.log("Killing the Port");
+//   process.kill(process.pid, "SIGINT");
+// });
 
 module.exports = app;
 
