92000e8d40ee8020f618613e17a4af2e841f3d70
Saad <rishat.5081@gmail.com>
2022-07-18 18:33:36 +0500

    add checks to the seeding files

--- Database_Operations/Seeding/User.seed.js
+++ Database_Operations/Seeding/User.seed.js
@@ -1,22 +1,32 @@
+require("dotenv").config();
 const RoleModel = require("../../models/Role");
 const UserModel = require("../../models/User");
 const userData = require("../Raw Data/user");
 const { color, log } = require("console-log-colors");
-
+require("../../config/Database/mongodb");
 (async () => {
   const roleData = await RoleModel.findOne({ roleName: "player" }).select(
     "_id"
   );
-  const userArray = await userData.userData;
-  const seeding = await UserModel.create(
-    userArray.map((user) => ({ ...user, roleId: roleData?._id }))
-  );
-  if (seeding.length > 0) {
-    log(color.cyan(" ******************************************** "));
-    log(color.cyan(" *******                              ******* "));
-    log(color.cyan(" ******* User   Inserted successfully ******* "));
-    log(color.cyan(" *******                              ******* "));
-    log(color.cyan(" ******************************************** "));
+  if (roleData?._id) {
+    const userArray = await userData.userData;
+    const seeding = await UserModel.create(
+      userArray.map((user) => ({ ...user, roleId: roleData?._id }))
+    );
+    if (seeding.length > 0) {
+      log(color.green(" ******************************************** "));
+      log(color.green(" *******                              ******* "));
+      log(color.green(" ******* User   Inserted successfully ******* "));
+      log(color.green(" *******                              ******* "));
+      log(color.green(" ******************************************** "));
+      process.exit();
+    }
+  } else {
+    log(color.red(" ******************************************** "));
+    log(color.red(" *******                              ******* "));
+    log(color.red(" ******* Run   `npm run role`   first ******* "));
+    log(color.red(" *******                              ******* "));
+    log(color.red(" ******************************************** "));
     process.exit();
   }
 })();
