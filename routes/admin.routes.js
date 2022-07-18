5c0d1d94aeaaf03d574be4fbd946ed3a344c5902
Saad <rishat.5081@gmail.com>
2022-07-18 15:35:48 +0500

    add the generate token to the route

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,9 +2,22 @@ const router = require("express").Router();
 const generalResponse = require("../utlls/response");
 const httpCodes = require("../utlls/httpCodestatus");
 const UserModel = require("../models/User");
+const GenerateToken = require("../helper/generateToken");
+/**
+ *
+ *
+ */
 router.get("/", (req, res) => {
   res.render("login");
 });
+
+/***
+ *
+ *
+ *
+ *
+ * Admin Login
+ */
 
 router.post("/login", async (req, res) => {
   try {
@@ -14,13 +27,17 @@ router.post("/login", async (req, res) => {
         message: "Email & Password is required",
       });
     } else {
-      const adminLogin = await UserModel.findOne({ email, password }).select(
-        "-password"
-      );
+      const adminLogin = await UserModel.findOne({ email, password })
+        .select("-password")
+        .populate("roleId", "roleName");
       if (adminLogin?._id) {
+        const token = await GenerateToken.generateAccess_RefreshToken(
+          adminLogin
+        );
         generalResponse(res, httpCodes.OK, {
           message: "Logged In Successfull",
           adminLogin,
+          token,
         });
       } else {
         generalResponse(res, httpCodes.BAD_REQUEST, {
