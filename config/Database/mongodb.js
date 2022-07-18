b798ed8ac217857dc70fd84517584e8dba601d51
Saad <rishat.5081@gmail.com>
2022-07-18 18:32:46 +0500

    add the console

--- config/Database/mongodb.js
+++ config/Database/mongodb.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const { color, log } = require('console-log-colors');
-
+const { color, log } = require("console-log-colors");
 
 mongoose.connect(process.env.mongoUrl, {
   useNewUrlParser: true,
@@ -8,10 +7,10 @@ mongoose.connect(process.env.mongoUrl, {
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
-db.once("open",  () =>{
-  log(color.cyan(' ******************************************** '));
-  log(color.cyan(' *******                              ******* '));
-  log(color.cyan(' ******* Mongo Connected successfully ******* '));
-  log(color.cyan(' *******                              ******* '));
-  log(color.cyan(' ******************************************** '));
+db.once("open", () => {
+  log(color.cyan(" ******************************************** "));
+  log(color.cyan(" *******                              ******* "));
+  log(color.cyan(" ******* Mongo Connected successfully ******* "));
+  log(color.cyan(" *******                              ******* "));
+  log(color.cyan(" ******************************************** "));
 });
