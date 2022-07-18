92c2814ab697e753a8d41e60c562348a194d9de5
Saad <rishat.5081@gmail.com>
2022-07-18 18:33:24 +0500

    add checks to the seeding files

--- Database_Operations/Seeding/Admin.seed.js
+++ Database_Operations/Seeding/Admin.seed.js
@@ -1,20 +1,31 @@
+require("dotenv").config();
 const RoleModel = require("../../models/Role");
 const UserModel = require("../../models/User");
-const adminData = require("../Raw Data/user");
+const adminData = require("../Raw Data/admin");
 const { color, log } = require("console-log-colors");
-
+require("../../config/Database/mongodb");
 (async () => {
   const roleData = await RoleModel.findOne({ roleName: "admin" }).select("_id");
-  const userArray = await adminData.adminData;
-  const seeding = await UserModel.create(
-    userArray.map((user) => ({ ...user, roleId: roleData?._id }))
-  );
-  if (seeding.length > 0) {
-    log(color.cyan(" ******************************************** "));
-    log(color.cyan(" *******                              ******* "));
-    log(color.cyan(" ******* Admin  Inserted successfully ******* "));
-    log(color.cyan(" *******                              ******* "));
-    log(color.cyan(" ******************************************** "));
+
+  if (roleData?._id) {
+    const userArray = await adminData.adminData;
+    const seeding = await UserModel.create(
+      userArray.map((user) => ({ ...user, roleId: roleData?._id }))
+    );
+    if (seeding.length > 0) {
+      log(color.green(" ******************************************** "));
+      log(color.green(" *******                              ******* "));
+      log(color.green(" ******* Admin  Inserted successfully ******* "));
+      log(color.green(" *******                              ******* "));
+      log(color.green(" ******************************************** "));
+      process.exit();
+    }
+  } else {
+    log(color.red(" ******************************************** "));
+    log(color.red(" *******                              ******* "));
+    log(color.red(" ******* Run `npm run role`     first ******* "));
+    log(color.red(" *******                              ******* "));
+    log(color.red(" ******************************************** "));
     process.exit();
   }
 })();
