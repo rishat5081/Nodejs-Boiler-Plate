853b6860923d2f17060b2fc62c19a65450b3ed7a
Saad <rishat.5081@gmail.com>
2022-07-18 18:33:31 +0500

    add checks to the seeding files

--- Database_Operations/Seeding/Role.seed.js
+++ Database_Operations/Seeding/Role.seed.js
@@ -1,16 +1,17 @@
+require("dotenv").config();
 const RoleModel = require("../../models/Role");
 const roleData = require("../Raw Data/roles");
 const { color, log } = require("console-log-colors");
-
+require("../../config/Database/mongodb");
 (async () => {
   const seeding = await RoleModel.create(roleData.rolesData);
 
   if (seeding.length > 0) {
-    log(color.cyan(" ******************************************** "));
-    log(color.cyan(" *******                              ******* "));
-    log(color.cyan(" ******* Roles Inserted  successfully ******* "));
-    log(color.cyan(" *******                              ******* "));
-    log(color.cyan(" ******************************************** "));
+    log(color.green(" ******************************************** "));
+    log(color.green(" *******                              ******* "));
+    log(color.green(" ******* Roles Inserted  successfully ******* "));
+    log(color.green(" *******                              ******* "));
+    log(color.green(" ******************************************** "));
     process.exit();
   }
 })();
