656a794d0c32055b52d24b7cfdd8d5f441f3d68c
Saad <rishat.5081@gmail.com>
2022-07-18 15:35:29 +0500

    generate Token with the role

--- helper/generateToken.js
+++ helper/generateToken.js
@@ -1,15 +1,36 @@
 const jwt = require("jsonwebtoken");
-
 module.exports = {
   generateAccess_RefreshToken: (user) => {
     return new Promise((resolve, reject) => {
       var token = {};
-      token.accessToken = jwt.sign(user, process.env.TOKEN_KEY, {
-        expiresIn: "5m",
-      });
-      refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_KEY, {
-        expiresIn: "6h",
-      });
+      token.accessToken = jwt.sign(
+        {
+          _id: user?._id,
+          first_name: user?.first_name,
+          last_name: user?.last_name,
+          email: user?.email,
+          roleId: user?.roleId,
+          roleName: user?.roleId[0].roleName,
+        },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "5m",
+        }
+      );
+      refreshToken = jwt.sign(
+        {
+          _id: user?._id,
+          first_name: user?.first_name,
+          last_name: user?.last_name,
+          email: user?.email,
+          roleId: user?.roleId,
+          roleName: user?.roleId[0].roleName,
+        },
+        process.env.REFRESH_TOKEN_KEY,
+        {
+          expiresIn: "6h",
+        }
+      );
       if (refreshToken) resolve({ status: true, token });
       else reject({ status: false, message: "Error creating the Tokens" });
     });
