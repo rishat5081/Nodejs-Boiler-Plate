e42780273cfd46b12d8e8eabca3d7c2aa2f871dc
Saad <rishat.5081@gmail.com>
2022-07-19 19:17:08 +0500

    adding the check to the main file

--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,22 @@ const router = require("express").Router();
 const User = require("./user.routes");
 const GeneralRoutes = require("./default.routes");
 
+const verifyToken = require("../middleware/auth");
+const requestBodyValidation = require("../middleware/requestBodyValidation");
+const accessControlValidation = require("../middleware/accessControl");
+
 /***
  *
  * Setting the routes
  */
 router.use("/", GeneralRoutes);
 
-router.use("/user", User);
+router.use(
+  "/user",
+  verifyToken,
+  accessControlValidation.allowIfLoggedin,
+  accessControlValidation.grantAccess("readOwn", "profile"),
+  User
+);
 
 module.exports = router;
