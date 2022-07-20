d69ed8b720a402c4b6ded7eaccebad130bc53f61
Saad <rishat.5081@gmail.com>
2022-07-20 14:47:21 +0500

    add the location util function

---
+++ utlls/fetchLocation.helper.js
@@ -0,0 +1,11 @@
+const geoip = require("geoip-lite");
+module.exports = {
+  fetchLocationUsingIP: async (ipAddress) => {
+    return new Promise((resolve, reject) => {
+      const location = geoip.lookup(ipAddress);
+      if (Object.keys(location).length > 0)
+        resolve({ status: true, latitude: geo.ll[0], longitude: geo.ll[1] });
+      else resolve({ status: false, latitude: "local", longitude: "local" });
+    });
+  },
+};
