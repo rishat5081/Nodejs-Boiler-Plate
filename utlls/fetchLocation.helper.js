1dfd67be5697e9887127c3fa50237cc15f1d1122
Saad <rishat.5081@gmail.com>
2022-07-20 14:53:37 +0500

    update the condition for the local ip in dev env

--- utlls/fetchLocation.helper.js
+++ utlls/fetchLocation.helper.js
@@ -3,7 +3,7 @@ module.exports = {
   fetchLocationUsingIP: async (ipAddress) => {
     return new Promise((resolve, reject) => {
       const location = geoip.lookup(ipAddress);
-      if (Object.keys(location).length > 0)
+      if (location !== null && Object.keys(location).length > 0)
         resolve({ status: true, latitude: geo.ll[0], longitude: geo.ll[1] });
       else resolve({ status: false, latitude: "local", longitude: "local" });
     });
