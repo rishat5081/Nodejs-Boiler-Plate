26556ebfaa37a8c0153c7efdd6f90cbfe67ebabd
Saad <rishat.5081@gmail.com>
2022-07-21 19:57:19 +0500

    add the multer

---
+++ config/Multer/multer.js
@@ -0,0 +1,33 @@
+let multer = require("multer"),
+  multerStorageObject = multer.diskStorage({
+    destination: "./public/userprofileImage/",
+    filename: (req, file, cb) => {
+      const fileBreakdown = file.mimetype.split("/");
+
+      if (fileBreakdown[0] === "image") {
+        cb(null, "API" + Date.now() + "." + fileBreakdown[1]);
+      } else cb(null, false, { message: req.flash("Wrong File type") });
+    },
+  });
+
+function checkFileType(file, cb) {
+  const types = /jpeg|jpg|png|gif/;
+  const fileBreakdown = file.mimetype.split("/");
+  const extnames = types.test(fileBreakdown[1].toLowerCase());
+  const mimetype = types.test(file.mimetype);
+
+  if (extnames && mimetype) return cb(null, true);
+  else cb("Wrong File Type");
+}
+
+const uploadImage = multer({
+  storage: multerStorageObject,
+  limits: {
+    fileSize: 3000000,
+  },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+}).any();
+
+module.exports = uploadImage;
