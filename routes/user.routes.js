e7bc7c47e30074fe1bedabc965f470ba49646a16
Saad <rishat.5081@gmail.com>
2022-07-21 16:09:15 +0500

    add the swagger to the update request

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -50,6 +50,95 @@ router.get(
  *    security:
  *      - auth: []
  *    description: Refresh Token Route
+ *    parameters:
+ *      - name: email
+ *        description: Email to use for login.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: test@gmail.com
+ *      - name: password
+ *        description: User's password.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: test@12345
+ *      - name: first_name
+ *        description: First Name of the User.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: John
+ *      - name: last_name
+ *        description:  Last Name of the User.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: Doe
+ *      - name: address
+ *        description: Address of the User.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: House 6 Street 23 DHA Phase 76 Islamabad
+ *      - name: contactNumber
+ *        description:  Contact Number of the User.
+ *        in: formData
+ *        type: number
+ *        schema:
+ *          type: number
+ *          maximum: 50
+ *          example: 6501
+ *      - name: gender
+ *        description:  Gender Can only be male, female, others.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: male, female, others
+ *      - name: dateOfBirth
+ *        description:  Date of Birth.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: date
+ *      - name: state
+ *        description:  State of your current location.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: Punjab
+ *      - name: city
+ *        description:  city of your current location.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: Islamabad
+ *      - name: zipCode
+ *        description:  Zip Code of your place.
+ *        in: formData
+ *        type: string
+ *        schema:
+ *          type: string
+ *          maximum: 50
+ *          example: zip
  *    responses:
  *      200:
  *        description: Success
