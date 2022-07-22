2e5d082cf30809539e44d7cd1c4025d2387339d4
Saad <rishat.5081@gmail.com>
2022-07-22 14:32:33 +0500

    add the login controller of the admin and also fix the issues of the login of admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const generalResponse = require("../utlls/response");
-const httpCodes = require("../utlls/httpCodestatus");
+const httpCodestatus = require("../utlls/httpCodestatus");
 const UserModel = require("../models/User");
-const GenerateToken = require("../helper/generateToken");
+const bcryptHelper = require("../utlls/bcrypt.helper"),
+  generateToken = require("../helper/generateToken");
 /**
  *
  *
@@ -22,32 +23,41 @@ router.get("/", (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!email && !password) {
-      generalResponse(res, httpCodes.BAD_REQUEST, {
-        message: "Email & Password is required",
+    const user = await UserModel.findOne({ email }).populate(
+      "roleId",
+      "roleName"
+    );
+    if (!user) {
+      generalResponse.errorResponse(res, httpCodestatus.INTERNAL_SERVER_ERROR, {
+        status: false,
+        message: "User does not exist",
+      });
+    } else if (!user?.roleId?.roleName.toString() === "admin") {
+      generalResponse.errorResponse(res, httpCodestatus.BAD_REQUEST, {
+        message: "User is not an Admin",
       });
     } else {
-      const adminLogin = await UserModel.findOne({ email, password })
-        .select("-password")
-        .populate("roleId", "roleName");
-      if (adminLogin?._id) {
-        const token = await GenerateToken.generateAccess_RefreshToken(
-          adminLogin
-        );
-        generalResponse(res, httpCodes.OK, {
-          message: "Logged In Successfull",
-          adminLogin,
-          token,
+      if (!(await bcryptHelper.compareHashPassword(password, user.password))) {
+        generalResponse.errorResponse(res, httpCodestatus.BAD_REQUEST, {
+          message: "Invalid Password",
         });
       } else {
-        generalResponse(res, httpCodes.BAD_REQUEST, {
-          message: "No User Found",
+        let accessToken = await generateToken.generateAccess_RefreshToken(user);
+        var clone = Object.assign({}, user);
+        delete clone._doc.password;
+        generalResponse.successResponse(res, httpCodestatus.OK, {
+          status: true,
+          message: "Logged In Successfully",
+          ...accessToken,
+          user: clone._doc,
         });
       }
     }
-  } catch (error) {
-    generalResponse(res, httpCodes.BAD_REQUEST, {
-      error,
+  } catch (err) {
+    console.log("--", err);
+    generalResponse.errorResponse(res, httpCodestatus.INTERNAL_SERVER_ERROR, {
+      message: "Error Login In User",
+      serverError: err,
     });
   }
 });
